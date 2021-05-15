4e93c29415e803b6a92e202f782209a44c91ca15
your username <59005110+pappu8871@users.noreply.github.com>
2021-05-15 15:11:22 +0530

    Added:Welcome file in main

---
+++ LineCompair.java
@@ -0,0 +1,9 @@
+package lineComparison;
+
+public class LineCompareWelcome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       System.out.println("Welcome to Line Comparison Practice Problem");
+	}
+}
