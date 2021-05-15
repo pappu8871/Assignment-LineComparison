f0f9cd4b95b16b6af97d116dc2cc338c691b5382
your username <59005110+pappu8871@users.noreply.github.com>
2021-05-15 15:15:55 +0530

    Added:LineCompareUC1.java

--- LineCompair.java
+++ LineCompair.java
@@ -1,9 +1,18 @@
 package lineComparison;
 
-public class LineCompareWelcome {
+import java.util.Scanner;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-       System.out.println("Welcome to Line Comparison Practice Problem");
-	}
+public class LineCompareUC1 {
+	public static void main(String[] args){
+		Scanner scn = new Scanner(System.in);
+		
+		System.out.println(" Enter the value for first Line X1, X2, Y1, Y2 " );
+		int x1 = scn.nextInt();
+		int x2 = scn.nextInt();
+		int y1 = scn.nextInt();
+		int y2 = scn.nextInt();
+	     
+	    double line1 = Math.sqrt((x2 - x1) * ( x2 - x1 ) + ( y2 - y1 ) * ( y2 - y1 ) );	 	    
+       System.out.println(" Line1: " +line1);
+}
 }
