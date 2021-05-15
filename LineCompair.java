c9652369b029acca189f1e2c77c4bf6efbf45910
your username <59005110+pappu8871@users.noreply.github.com>
2021-05-15 15:36:22 +0530

    Added:class LineCompare.java use compareTo

--- LineCompair.java
+++ LineCompair.java
@@ -2,7 +2,8 @@ package lineComparison;
 
 import java.util.Scanner;
 
-public class LineCompareUC2 {
+class LineCompareUC3 {
+
 	public static void main(String[] args) {
 		Scanner scn = new Scanner(System.in);
 		
@@ -18,17 +19,19 @@ public class LineCompareUC2 {
 		int b1 = scn.nextInt();
 		int b2 = scn.nextInt();
 	     
-	     double line1 = Math.sqrt((x2 - x1) * ( x2 - x1 ) + ( y2 - y1 ) * ( y2 - y1 ) );	 
+	    Double line1 =  Math.sqrt((x2 - x1) * ( x2 - x1 ) + ( y2 - y1 ) * ( y2 - y1 ) );	 
 	     System.out.println(" Line1: " +line1);
 	     
-	     double line2 = Math.sqrt((a2 - a1) * ( a2 - a1 ) + ( b2 - b1 ) * ( b2 - b1 ) );	 
+	    Double line2 =  Math.sqrt((a2 - a1) * ( a2 - a1 ) + ( b2 - b1 ) * ( b2 - b1 ) );	 
 	     System.out.println(" Line2: " +line2);
 	     
-	     if (line1 == line2) {
-         	System.out.println("Both lines are equals");
-         }
-         else 
-           System.out.println("lines are not equals");
-	     
-}
-}
+	     int compareValue = line1.compareTo(line2);
+         if (compareValue == 0)
+             System.out.println("line1 and line1 are equal");
+         else if (compareValue < 0)
+             System.out.println("lines1 is less than lines2");
+         else
+             System.out.println("lines1 is greater than lines2");
+	}
+	}
+
