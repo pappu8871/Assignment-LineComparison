9ba7c449d41d7a0fba6e414e6b3b2cb5fad40591
your username <59005110+pappu8871@users.noreply.github.com>
2021-05-15 15:30:35 +0530

    Added:LineCompareUC2.java

--- LineCompair.java
+++ LineCompair.java
@@ -2,8 +2,8 @@ package lineComparison;
 
 import java.util.Scanner;
 
-public class LineCompareUC1 {
-	public static void main(String[] args){
+public class LineCompareUC2 {
+	public static void main(String[] args) {
 		Scanner scn = new Scanner(System.in);
 		
 		System.out.println(" Enter the value for first Line X1, X2, Y1, Y2 " );
@@ -11,8 +11,24 @@ public class LineCompareUC1 {
 		int x2 = scn.nextInt();
 		int y1 = scn.nextInt();
 		int y2 = scn.nextInt();
+		
+		System.out.println(" Enter the value for first Line a1, a2, b1, b2 " );
+		int a1 = scn.nextInt();
+		int a2 = scn.nextInt();
+		int b1 = scn.nextInt();
+		int b2 = scn.nextInt();
 	     
-	    double line1 = Math.sqrt((x2 - x1) * ( x2 - x1 ) + ( y2 - y1 ) * ( y2 - y1 ) );	 	    
-       System.out.println(" Line1: " +line1);
+	     double line1 = Math.sqrt((x2 - x1) * ( x2 - x1 ) + ( y2 - y1 ) * ( y2 - y1 ) );	 
+	     System.out.println(" Line1: " +line1);
+	     
+	     double line2 = Math.sqrt((a2 - a1) * ( a2 - a1 ) + ( b2 - b1 ) * ( b2 - b1 ) );	 
+	     System.out.println(" Line2: " +line2);
+	     
+	     if (line1 == line2) {
+         	System.out.println("Both lines are equals");
+         }
+         else 
+           System.out.println("lines are not equals");
+	     
 }
 }
